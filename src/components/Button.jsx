c227138289ff3f3a545a653aba3f6740291db720
saschadueser <saschadueser@gmail.com>
2025-10-29 18:18:09 +0100

    Modified button - better animation on hover

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,33 @@
 import '../styles/Button.css';
 
 export default function Button(props) {
+
+    function buttonHoverIn(e) {
+        e.currentTarget.classList.add("hoverIn");
+    }
+
+    function buttonHoverOut(e) {
+        const el = e.currentTarget;
+            el.classList.remove("hoverIn");
+            el.classList.add("hoverOut");
+            setTimeout(() => {
+                el.style.transition = "none"
+                el.classList.remove("hoverOut")
+                setTimeout(() => {
+                    el.style.transition = "300ms ease-in"
+                }, 1);
+            }, 300);
+    }
+
     return (
-        <a className={"button"} href={props.buttonLink} target={props.newTab ? "_blank" : "_self"}>{props.buttonContent} {props.icon ? <span className={`fa ${props.icon}`}></span> : ""}</a>
+        <a 
+        className={"button"} 
+        href={props.buttonLink} 
+        target={props.newTab ? "_blank" : "_self"}
+        onMouseEnter={buttonHoverIn}
+        onMouseLeave={buttonHoverOut}
+        >
+            {props.buttonContent} {props.icon ? <span className={`fa ${props.icon}`}></span> : ""}
+        </a>
     )
 }
